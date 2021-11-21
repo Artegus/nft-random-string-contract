b6df50c6cb31e22640ad11733a1fb9b744d7e5cf
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2021-11-21 18:48:27 +0100

    feat: Added contract RS

---
+++ contracts/randomString.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import {Base64} from "./libraries/Base64.sol";
+
+contract randomString is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 public constant TOTAL_SUPPLY = 50;
+
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] firstWords = ["Cripy", "Satoshi", "Kid", "Web", "UI", "Usb"];
+    string[] secondWords = [
+        "Bottle",
+        "Red",
+        "Couch",
+        "Helmet",
+        "Bottle",
+        "Cloud"
+    ];
+    string[] thirdWords = [
+        "Blue",
+        "Clown",
+        "Square",
+        "Ransom",
+        "Led",
+        "Script"
+    ];
+
+    event RandomStringMinted(address sender, uint256 tokenId);
+
+    constructor() ERC721("RandomStringsAF", "RDAF") {
+    }
+
+    function pickRandomFirstWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
+
+    function getTotalMinted() external view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    function makeARandomString() public {
+        uint256 newItemId = _tokenIds.current();
+
+        require(newItemId < TOTAL_SUPPLY, "All available items have been minted!");
+
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+        string memory combinedWord = string(
+            abi.encodePacked(first, second, third)
+        );
+
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, combinedWord, "</text></svg>")
+        );
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        combinedWord,
+                        '", "description": "Random Strings on the Rinkeby test network ", "image": "data:image/svg+xml;base64,',
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, finalTokenUri);
+
+        _tokenIds.increment();
+
+        emit RandomStringMinted(msg.sender, newItemId);
+    }
+}
